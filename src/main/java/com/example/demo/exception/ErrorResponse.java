1bbbd26a15292113ae5f37b38c6fca4f2ce0334b
Thiannight <thianntatiana@gmail.com>
2026-06-05 10:23:43 +0300

    Feat/exception (#6)
    * feat: error exception
    
    * feat: create GlobalExceptionHandler
    
    * style: reformat code to match Google style guide
    
    * style: fix code formatting
    
    * style: fix code formatting
    
    * fix: prevent NullPointerException in handleTypeMismatch
    
    * style: fix code formatting

---
+++ src/main/java/com/example/demo/exception/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.example.demo.exception;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+  private int status;
+  private String error;
+  private String message;
+  private LocalDateTime timestamp;
+}
