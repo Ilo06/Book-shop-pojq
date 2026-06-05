7180786eb3371f21d5733e2643396d0e70e4e83d
Thiannight <thianntatiana@gmail.com>
2026-06-05 21:32:13 +0300

    feat: validation in CreateAuthorDTO and CreateGenreDTO

--- src/main/java/com/example/demo/dto/request/CreateAuthorDTO.java
+++ src/main/java/com/example/demo/dto/request/CreateAuthorDTO.java
@@ -1,11 +1,20 @@
 package com.example.demo.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class CreateAuthorDTO {
+  @NotBlank
+  @Size(min = 1, max = 100)
   private String firstName;
+
+  @NotBlank
+  @Size(min = 1, max = 100)
   private String lastName;
 }
