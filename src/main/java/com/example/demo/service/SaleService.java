1d7cd45f1014b84b5729c635134f9cc961402e3f
RRTRyan <tiaryanrzk@gmail.com>
2026-06-06 19:45:03 +0300

    feat: add SaleController

--- src/main/java/com/example/demo/service/SaleService.java
+++ src/main/java/com/example/demo/service/SaleService.java
@@ -1,48 +1,51 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.BookCopy;
 import com.example.demo.entity.Sale;
-import com.example.demo.entity.SaleBookCopy;
+import com.example.demo.exception.ResourceConflictException;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.bookshop.SaleBookCopyRepository;
 import com.example.demo.repository.bookshop.SaleRepository;
-import jakarta.ws.rs.BadRequestException;
-import jakarta.ws.rs.NotFoundException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SaleService {
-    private SaleRepository saleRepository;
-    private SaleBookCopyRepository saleBookCopyRepository;
+  private final SaleRepository saleRepository;
+  private final SaleBookCopyRepository saleBookCopyRepository;
 
-    public List<Sale> findByDateBetween(LocalDate from, LocalDate to) {
-        return saleRepository.findBySaleDateBetween(from, to);
-    }
+  public List<Sale> findByDateBetween(LocalDate from, LocalDate to) {
+    return saleRepository.findBySaleDateBetween(from, to);
+  }
 
-    public Sale save(Sale sale) {
-        sale.getBooks().forEach(bookCopy -> {
-            if (saleBookCopyRepository.existsByBookCopyId(((bookCopy.getBookCopy().getId())))) {
-                throw new BadRequestException("Book copy has already been sold");
-            }
-        });
-        saleRepository.save(sale);
-        saleBookCopyRepository.saveAll(sale.getBooks());
-        return sale;
-    }
+  public List<Sale> getAll() {
+    return saleRepository.findAll();
+  }
 
-    public Sale findById(UUID id) {
-        Optional<Sale> optionalSale = saleRepository.findById(id);
-        if (optionalSale.isEmpty()) {
-            throw new NotFoundException("Sale with id " + id + " not found");
-        }
-        Sale sale = optionalSale.get();
-        sale.setBooks(saleBookCopyRepository.findBySaleId(sale.getId()));
-        return sale;
+  public Sale save(Sale sale) {
+    sale.getBooks()
+        .forEach(
+            bookCopy -> {
+              if (saleBookCopyRepository.existsByBookCopyId(((bookCopy.getBookCopy().getId())))) {
+                throw new ResourceConflictException("Book copy has already been sold");
+              }
+            });
+    saleRepository.save(sale);
+    saleBookCopyRepository.saveAll(sale.getBooks());
+    return sale;
+  }
+
+  public Sale findById(UUID id) {
+    Optional<Sale> optionalSale = saleRepository.findById(id);
+    if (optionalSale.isEmpty()) {
+      throw new ResourceNotFoundException("Sale with id " + id + " not found");
     }
+    Sale sale = optionalSale.get();
+    sale.setBooks(saleBookCopyRepository.findBySaleId(sale.getId()));
+    return sale;
+  }
 }
