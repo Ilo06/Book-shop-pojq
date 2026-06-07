6b50a2ad31c85aee8889f80f5c4f7f83ac6bdfd3
RRTRyan <tiaryanrzk@gmail.com>
2026-06-07 19:56:43 +0300

    fix: uniformize responses and use DTO

--- src/main/java/com/example/demo/endpoint/rest/controller/bookshop/SaleController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/bookshop/SaleController.java
@@ -1,10 +1,12 @@
 package com.example.demo.endpoint.rest.controller.bookshop;
 
-import com.example.demo.entity.Sale;
+import com.example.demo.dto.request.CreateSaleDTO;
+import com.example.demo.dto.response.SaleResponse;
 import com.example.demo.service.SaleService;
 import java.time.LocalDate;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,16 +20,16 @@ public class SaleController {
   public ResponseEntity<?> getAll(
       @RequestParam(required = false) LocalDate from,
       @RequestParam(required = false) LocalDate to) {
-    return ResponseEntity.ok(saleService.findByDateBetween(from, to));
+    return ResponseEntity.status(HttpStatus.OK).body(saleService.findByDateBetween(from, to));
   }
 
   @PostMapping
-  public ResponseEntity<?> save(@RequestBody Sale sale) {
-    return ResponseEntity.ok(saleService.save(sale));
+  public ResponseEntity<?> save(@RequestBody CreateSaleDTO sale) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(saleService.save(sale));
   }
 
   @GetMapping("/{saleId}")
   public ResponseEntity<?> getById(@PathVariable UUID saleId) {
-    return ResponseEntity.ok(saleService.findById(saleId));
+    return ResponseEntity.status(HttpStatus.OK).body(saleService.findById(saleId));
   }
 }
