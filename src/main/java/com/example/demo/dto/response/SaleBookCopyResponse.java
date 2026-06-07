af7da53477284f6e5c874ddf43730de106254e55
RRTRyan <tiaryanrzk@gmail.com>
2026-06-07 19:51:35 +0300

    feat: add response DTO

---
+++ src/main/java/com/example/demo/dto/response/SaleBookCopyResponse.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto.response;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleBookCopyResponse {
+  private UUID bookCopyId;
+  private BigDecimal price;
+}
