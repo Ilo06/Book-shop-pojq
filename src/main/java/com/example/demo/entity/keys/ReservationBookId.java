b75ad3a081f0d5c3d3c7dd104b1ea5b6eb288cc3
Thiannight <thianntatiana@gmail.com>
2026-06-07 14:59:49 +0300

    feat: add constructors to composite keys

--- src/main/java/com/example/demo/entity/keys/ReservationBookId.java
+++ src/main/java/com/example/demo/entity/keys/ReservationBookId.java
@@ -3,8 +3,14 @@ package com.example.demo.entity.keys;
 import jakarta.persistence.Embeddable;
 import java.io.Serializable;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class ReservationBookId implements Serializable {
   private UUID reservationId;
   private UUID bookId;
