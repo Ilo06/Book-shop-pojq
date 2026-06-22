3b95aab744e7862c2bfe3123846fae866e489cd3
RRTRyan <tiaryanrzk@gmail.com>
2026-06-22 07:48:36 +0300

    fix(sql): user missing role column

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -24,7 +24,8 @@ CREATE TYPE user_role           AS ENUM ('USER', 'ADMIN');
 CREATE TABLE users (
     id          UUID            DEFAULT gen_random_uuid() PRIMARY KEY,
     username    VARCHAR(255)    NOT NULL UNIQUE,
-    password    VARCHAR(255)    NOT NULL
+    password    VARCHAR(255)    NOT NULL,
+    role        user_role       NOT NULL DEFAULT 'USER'
 );
 
 CREATE TABLE author (
