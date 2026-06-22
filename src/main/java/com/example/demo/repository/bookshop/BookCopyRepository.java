515bf012b5836470d541e8592d68415623a87d5b
RRTRyan <tiaryanrzk@gmail.com>
2026-06-22 10:34:09 +0300

    feat: add book management methods in BookCopyRepository

--- src/main/java/com/example/demo/repository/bookshop/BookCopyRepository.java
+++ src/main/java/com/example/demo/repository/bookshop/BookCopyRepository.java
@@ -1,12 +1,14 @@
 package com.example.demo.repository.bookshop;
 
 import com.example.demo.entity.BookCopy;
+import com.example.demo.entity.enums.BookCopyType;
 import com.example.demo.entity.enums.BookStatus;
 import com.example.demo.repository.projection.StockProjection;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -27,6 +29,27 @@ public interface BookCopyRepository extends JpaRepository<BookCopy, UUID> {
       GROUP BY b.id, b.title
       """)
   List<StockProjection> findAvailableCopiesPerBook();
+
+  @Query(
+    """
+    SELECT COUNT(bc) AS copies
+    FROM BookCopy AS bc
+    JOIN bc.book AS b
+    WHERE b.id = :bookId
+    """
+  )
+  Integer findAvailableCopiesPerBookId(@Param("bookId") UUID bookId);
+
+  @Query(
+          """
+          SELECT COUNT(bc) AS copies
+          FROM BookCopy AS bc
+          JOIN bc.book AS b
+          WHERE b.id = :bookId AND bc.type = LOWER(CAST(:bookType AS string))
+          """
+  )
+  Integer findAvailableCopiesTypePerBookId(@Param("bookId") UUID bookId,
+                              @Param("bookType") BookCopyType bookCopyType);
 
   @Query(
       """
