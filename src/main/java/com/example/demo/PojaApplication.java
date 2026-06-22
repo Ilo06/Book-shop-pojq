6c117a5c37ac52e87c39bfaf4caa92cbe29dc99e
Thiannight <thianntatiana@gmail.com>
2026-06-22 10:46:48 +0300

    chore: format

--- src/main/java/com/example/demo/PojaApplication.java
+++ src/main/java/com/example/demo/PojaApplication.java
@@ -1,9 +1,8 @@
 package com.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @PojaGenerated
