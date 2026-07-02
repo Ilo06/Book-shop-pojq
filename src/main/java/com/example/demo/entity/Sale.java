6e007a4cf87326f45efdd1ea8d865885ecf076f9
RRTRyan <tiaryanrzk@gmail.com>
2026-07-02 18:58:27 +0300

    fix!: update sale to use Instant and have status

--- src/main/java/com/example/demo/entity/Sale.java
+++ src/main/java/com/example/demo/entity/Sale.java
@@ -1,7 +1,9 @@
 package com.example.demo.entity;
 
+import com.example.demo.entity.enums.SaleStatus;
 import jakarta.persistence.*;
-import java.time.LocalDate;
+
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -24,9 +26,23 @@ public class Sale {
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
-  @Temporal(TemporalType.DATE)
+  @Enumerated(EnumType.STRING)
   @Column(nullable = false)
-  private LocalDate saleDate;
+  @Builder.Default
+  private SaleStatus status = SaleStatus.PENDING;
+
+  @Column(nullable = false)
+  @Builder.Default
+  private Boolean isReservation = Boolean.FALSE;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false)
+  @Builder.Default
+  private Instant creationDateTime = Instant.now();
+
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column
+  private Instant finalizationDateTime;
 
   @OneToMany(mappedBy = "sale", fetch = FetchType.LAZY)
   @Builder.Default
