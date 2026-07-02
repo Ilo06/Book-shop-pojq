ed9b6709c064bd15045514a39587a21bdc28d44b
RRTRyan <tiaryanrzk@gmail.com>
2026-07-02 18:53:22 +0300

    fix!: remove all reservation related classes

--- src/main/java/com/example/demo/entity/enums/BookStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.entity.enums;
-
-public enum BookStatus {
-  AVAILABLE,
-  SOLD_OUT
-}
--- src/main/java/com/example/demo/entity/enums/ReservationStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.entity.enums;
-
-public enum ReservationStatus {
-  PENDING,
-  CONFIRMED,
-  CANCELLED
-}
