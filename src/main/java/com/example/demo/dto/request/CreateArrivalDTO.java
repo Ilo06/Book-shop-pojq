ca943804a5ac386d2439924b2acf33b71c464527
RRTRyan <tiaryanrzk@gmail.com>
2026-07-09 11:15:27 +0300

    fix: replace invalid NotNull import with Jakarta Validation

--- src/main/java/com/example/demo/dto/request/CreateArrivalDTO.java
+++ src/main/java/com/example/demo/dto/request/CreateArrivalDTO.java
@@ -1,10 +1,10 @@
 package com.example.demo.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.jetbrains.annotations.NotNull;
 
 @Data
 @AllArgsConstructor
