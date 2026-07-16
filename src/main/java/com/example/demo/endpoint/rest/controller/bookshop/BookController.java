e919b6c9da8c6910204164770c88dcde4f71961b
RRTRyan <tiaryanrzk@gmail.com>
2026-07-16 20:38:02 +0300

    fix: move external search to own controller

--- src/main/java/com/example/demo/endpoint/rest/controller/bookshop/BookController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/bookshop/BookController.java
@@ -3,9 +3,7 @@ package com.example.demo.endpoint.rest.controller.bookshop;
 import com.example.demo.dto.request.CreateBookDTO;
 import com.example.demo.dto.response.BookResponse;
 import com.example.demo.dto.response.BookSummaryResponse;
-import com.example.demo.exception.BadGatewayException;
 import com.example.demo.service.BookService;
-import com.example.demo.service.ExternalSearchService;
 import jakarta.validation.Valid;
 import java.time.Instant;
 import java.util.List;
@@ -21,7 +19,6 @@ import org.springframework.web.bind.annotation.*;
 public class BookController {
 
   private final BookService bookService;
-  private final ExternalSearchService externalSearchService;
 
   @GetMapping
   public ResponseEntity<List<BookSummaryResponse>> listBooks(
@@ -38,13 +35,6 @@ public class BookController {
     return ResponseEntity.status(HttpStatus.OK)
         .header("Content-Type", "application/json")
         .body(bookService.findById(bookId));
-  }
-
-  @GetMapping(path = "/external-search", params = "isbn")
-  public ResponseEntity<?> findBookByISBN(@RequestParam String isbn) throws BadGatewayException {
-    return ResponseEntity.status(HttpStatus.OK)
-        .header("Content-Type", "application/json")
-        .body(externalSearchService.findBookByISBN(isbn));
   }
 
   @PostMapping
